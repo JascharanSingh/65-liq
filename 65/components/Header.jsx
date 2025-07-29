1cd56e4af03fe728f757f1a78119835e976f3aaa
Singh Technology <harryghotra195@gmail.com>
2025-07-29 18:23:25 -0400

    fixed header.jsx and created Header.css

--- 65/components/Header.jsx
+++ 65/components/Header.jsx
@@ -1,56 +1,44 @@
-// src/components/Header.jsx
 import React from "react";
 import { FaShoppingCart, FaStore } from "react-icons/fa";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="position-relative">
-      {/* Background image section */}
-      <div className="header-image-wrapper">
-        <img
-          src="/images/header-img.jpg"
-          alt="Whiskey Bottles"
-          className="img-fluid w-100 header-img"
-        />
-      </div>
+    <header className="position-relative vh-100 overflow-hidden">
+      {/* Background image */}
+      <img
+        src="/images/header-img.avif"
+        alt="Whiskey Bottles"
+        loading="lazy"
+        className="position-absolute top-0 start-0 w-100 h-100 object-fit-cover z-n1"
+      />
 
-      {/* Glass-style navbar on top of image */}
-      <div className="glass-navbar container d-flex align-items-center justify-content-between flex-wrap gap-3 py-2 px-4">
-        {/* Store logo image */}
+      {/* Glass-style navbar */}
+      <div className="glass-navbar container d-flex align-items-center justify-content-between flex-wrap gap-3 py-2 px-4 position-relative">
+        {/* Logo */}
         <img
           src="/images/LOGO.png"
           alt="65 Liquor Store Logo"
           className="img-fluid"
-          style={{ maxHeight: "70px", objectFit: "contain" }}
+          style={{
+            maxHeight: "70px",
+            objectFit: "contain",
+          }}
         />
 
-        {/* Search bar with blur */}
+        {/* Search bar */}
         <div className="flex-grow-1 mx-3">
           <div className="input-group input-group-sm">
-          <input
-  type="text"
-  className="form-control search-input"
-  placeholder="What can we help you find today?"
-  style={{
-    height: "34px",
-    fontSize: "0.875rem",
-    backgroundColor: "rgba(255, 255, 255, 0.2)",
-    backdropFilter: "blur(6px)",
-    WebkitBackdropFilter: "blur(6px)",
-    border: "1px solid rgba(255, 255, 255, 0.3)",
-    color: "white",
-  }}
-/>
-            <button
-              className="btn btn-sienna px-3"
-              style={{ fontSize: "0.875rem" }}
-            >
-              Search
-            </button>
+            <input
+              type="text"
+              className="form-control search-input text-white"
+              placeholder="What can we help you find today?"
+            />
+            <button className="btn btn-sienna px-3">Search</button>
           </div>
         </div>
 
-        {/* Store pickup info */}
+        {/* Pickup Info */}
         <div className="d-none d-md-flex align-items-center me-3 text-white">
           <FaStore className="me-2" />
           <div style={{ fontSize: "0.875rem" }}>
@@ -59,7 +47,7 @@ const Header = () => {
           </div>
         </div>
 
-        {/* Cart icon */}
+        {/* Cart Icon */}
         <FaShoppingCart className="text-white" size={18} />
       </div>
     </header>
