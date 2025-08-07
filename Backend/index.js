3fc61ecf5c6d0511f2ac4ee9f8d81bb331eeacf0
Singh Technology <harryghotra195@gmail.com>
2025-08-07 17:55:26 -0400

    afbdfbh

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ const app = express();
 // ✅ CORS Configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
